29d52beead2c1da5c05aa09fff9822477a0206f0
naina123mv <nainavaskenly@yahoo.com>
2019-10-26 00:21:24 +0530

    Made insertion sort recursive

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -2,23 +2,39 @@
 #include <vector>
 
 using namespace std;
-
+void insertionSortRecursive(int arr[], int n) 
+{ 
+    // Base case 
+    if (n <= 1) 
+        return; 
+  
+    // Sort first n-1 elements 
+    insertionSortRecursive( arr, n-1 ); 
+  
+    // Insert last element at its correct position 
+    // in sorted array. 
+    int last = arr[n-1]; 
+    int j = n-2; 
+  
+    /* Move elements of arr[0..i-1], that are 
+      greater than key, to one position ahead 
+      of their current position */
+    while (j >= 0 && arr[j] > last) 
+    { 
+        arr[j+1] = arr[j]; 
+        j--; 
+    } 
+    arr[j+1] = last; 
+} 
 int main(){
     int n,i,j,key;
     cin>>n;
-    vector<int> arr(n);
+   int arr[n];
     for(i=0;i<n;i++)
         cin>>arr[i];
-    for(j=1;j<arr.size();j++){
-        i=j-1;
-        key = arr[j];
-        while((i>=0)&&(arr[i]>key)){
-            arr[i+1]=arr[i];
-            i--;
-        }
-        arr[i+1]=key;
-    }
-    for(i=0;i<arr.size();i++)
+    insertionSortRecursive(arr,n)
+        ;
+    for(i=0;i<n;i++)
     	cout<<arr[i]<<" ";
     cout<<endl;
     return 0;
